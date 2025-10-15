9c4057f1cbe30e61da5bb9043d9c88e5ac0119e8
Simeon ~blueMan <soyebola@atb.tech>
2025-10-15 23:27:47 +0100

    Add background image and update API URL in environment settings

--- frontend/crimson-arc-frontend/src/environments/environment.ts
+++ frontend/crimson-arc-frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3900/api',
+//   apiUrl: 'http://localhost:3900/api',
+  apiUrl: 'https://stcoga-be.onrender.com/api',
 };
